9666f60842916de075a22ce228dc0b51984e95a2
Daniel Mazurok <dimazurok@cps.edu>
2025-09-23 11:01:22 -0500

    finished a2.py

--- a2.py
+++ a2.py
@@ -26,27 +26,46 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
 
         # 1) if we reached the end of the pattern but not source
         if pind == len(pattern):
-            print("reached end of pattern, but not source - return none")
+            # print("reached end of pattern, but not source - return none")
             return None
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
+        elif pattern[pind] == "%":
+            # Two situation, % is last, and % is not last
+            if pind == len(pattern) - 1:
+                combined = " ".join(source[sind:])
+                result.append(combined)
+                # print(result)
+                return result
+            else: # not the last element of pattern
+                pind += 1
+                slocation = sind
+                while pattern[pind] != source[sind]:
+                    sind += 1
+
+                    if sind == len(source):
+                        return None
+                    
+               #  print(sind, slocation)
+                result.append(" ".join(source[slocation:sind]))
+                # print result 
 
         # 3) if we reached the end of the source but not the pattern
         elif sind == len(source):
-            print("reached end of source, but not pattern - return none")
+           #  print("reached end of source, but not pattern - return none")
             return None
         # 4) if the current thing in the pattern is an _
         elif pattern[pind] == "_":
-            print(pattern[pind], source[sind])
+           # print(pattern[pind], source[sind])
             result.append(source[sind])
             pind += 1
             sind += 1
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
         elif pattern[pind] == source[sind]:
-            print(pattern[pind], source[sind])
+          #  print(pattern[pind], source[sind])
             pind += 1
             sind += 1
 
@@ -54,11 +73,11 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # indicates the current thing it pattern doesn't match the current thing in
         # source
         else:
-            print(pattern[pind] + " " + source[sind])
-            print("no match")
+           # print(pattern[pind] + " " + source[sind])
+           # print("no match")
             return None
 
-    print("successful match")
+    # print("successful match")
     return result
 
 
@@ -70,6 +89,8 @@ if __name__ == "__main__":
     assert match(["x", "_", "z"], ["x", "y", "z"]) == ["y"], "test 5 failed"
     assert match(["x", "_", "_"], ["x", "y", "z"]) == ["y", "z"], "test 6 failed"
     assert match(["%"], ["x", "y", "z"]) == ["x y z"], "test 7 failed"
+    assert match(["x", "%"], ["x", "y", "z", "a", "b"]) == ["y z a b"], "extra test 7 failed"
+
     assert match(["x", "%", "z"], ["x", "y", "z"]) == ["y"], "test 8 failed"
     assert match(["%", "z"], ["x", "y", "z"]) == ["x y"], "test 9 failed"
     assert match(["x", "%", "y"], ["x", "y", "z"]) == None, "test 10 failed"
